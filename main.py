ae1adbf23ea2f6d9567be51e9b0458d91dd835c5
Lusan Sapkota <91797475+Lusan-sapkota@users.noreply.github.com>
2024-04-04 14:14:27 +0545

    Update main.py

--- main.py
+++ main.py
@@ -137,4 +137,4 @@ def delete_todo(id):
     return redirect(url_for('todos'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
